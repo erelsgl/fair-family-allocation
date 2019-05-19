e8d06e5cffa9ed2950f46b68f158c59fb9df53b2
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-05-19 19:55:55 +0300

    remove bundles from Family object

--- rwav.py
+++ rwav.py
@@ -29,7 +29,6 @@ class BinaryFamily:
                                 to the agent's target value (- number of desired goods the family should have so that the agent is happy)
         """
         self.members = list(members)
-        self.allocated_goods = set()
         for member in self.members:
             member.target_value = fairness_criterion(member.total_value())
         self.trace = lambda *x: None     # to trace, set self.trace = print
@@ -77,26 +76,7 @@ class BinaryFamily:
         return "Allocated bundle = {}, happy members = {}/{}".format(
             bundle, self.num_of_happy_members(bundle), self.num_of_members())
 
-    def take_good(self, good:str):
-        """
-        Add the given good to the family's bundle.
-        """
-        self.allocated_goods.add(good)
-
-    def take_goods(self, goods:list):
-        """
-        Add the given goods to the family's bundle.
-        """
-        for good in goods:
-            self.allocated_goods.add(good)
-
-    def discard_goods(self):
-        """
-        Reset the family's bundle to an empty bundle.
-        """
-        self.allocated_goods = set()
-
-    def member_weight(self, member:BinaryAgent, remaining_goods:set)->float:
+    def member_weight(self, member:BinaryAgent, owned_goods:set, remaining_goods:set)->float:
         """
         Calculate the voting-weight of the given member with the given remaining goods.
 
@@ -104,13 +84,13 @@ class BinaryFamily:
         >>> Bob   = BinaryAgent({"w","x","y","z"})
         >>> fairness_1_of_2_mms = fairness_criteria.maximin_share_one_of_c(2)
         >>> family = BinaryFamily([Alice,Bob], fairness_1_of_2_mms)
-        >>> family.member_weight(Alice, {"x","y","z"})
+        >>> family.member_weight(Alice, set(), {"x","y","z"})
         0.5
-        >>> family.member_weight(Bob, {"x","y","z"})
+        >>> family.member_weight(Bob, set(), {"x","y","z"})
         0.375
         """
         member_remaining_value  = member.value(remaining_goods)               # the "r" of the member
-        member_current_value    = member.value(self.allocated_goods)
+        member_current_value    = member.value(owned_goods)
         member_should_get_value = member.target_value - member_current_value  # the "s" of the member
         member_weight = weight(member_remaining_value, member_should_get_value)
         self.trace("\t\t{} member{}\t\t{}\t\t{}\t{}\t{}".format(
@@ -119,7 +99,7 @@ class BinaryFamily:
             member_should_get_value, member_weight))
         return member_weight
 
-    def choose_good(self, remaining_goods:set)->str:
+    def choose_good(self, owned_goods:set, remaining_goods:set)->str:
         """
         Calculate the good that the family chooses from the set of remaining goods.
         It uses weighted-approval-voting.
@@ -128,14 +108,14 @@ class BinaryFamily:
         >>> agent2 = BinaryAgent({"z","w"})
         >>> fairness_1_of_best_2 = lambda r: 1 if r>=2 else 0
         >>> family = BinaryFamily([agent1,agent2], fairness_1_of_best_2)
-        >>> family.choose_good({"x","y","z"})
+        >>> family.choose_good(set(), {"x","y","z"})
         'z'
         """
         map_good_to_total_weight = defaultdict(int)
         self.trace("\tCalculating member weights:")
         self.trace("\t\t         \t\tDesired set\t\tr\ts\tWeight")
         for member in self.members:
-            member_weight           = self.member_weight(member, remaining_goods)
+            member_weight           = self.member_weight(member, owned_goods, remaining_goods)
             for good in member.desired_goods:
                 map_good_to_total_weight[good] += member_weight * member.cardinality
 
@@ -153,47 +133,48 @@ class BinaryFamily:
         return "\n".join([" * "+member.__repr__() for member in self.members])
 
 
-def allocate(family1: BinaryFamily, family2: BinaryFamily, goods: set):
+def allocate(families:list, goods: set)->list:
     """
-    Run the RWAV protocol on the two given families.
-    Set family1.allocated_goods and family2.allocated_goods to their final bundles.
+    Run the RWAV protocol on the given families.
+    Currently only 2 families are supported.
+    Return a list of bundles - a bundle per family.
 
     >>> fairness_1_of_best_2 = fairness_criteria.one_of_best_c(2)
     >>> family1 = BinaryFamily([BinaryAgent({"w","x"},1),BinaryAgent({"x","y"},2),BinaryAgent({"y","z"},3), BinaryAgent({"z","w"},4)], fairness_1_of_best_2)
     >>> family2 = BinaryFamily([BinaryAgent({"w","z"},2),BinaryAgent({"z","y"},3)], fairness_1_of_best_2)
-    >>> allocate(family1, family2, ["w","x","y","z"])
-    >>> sorted(family1.allocated_goods)
+    >>> (bundle1,bundle2) = allocate([family1, family2], ["w","x","y","z"])
+    >>> sorted(bundle1)
     ['x', 'z']
-    >>> sorted(family2.allocated_goods)
+    >>> sorted(bundle2)
     ['w', 'y']
     """
+    n_families = len(families)
+    if n_families!=2:
+        raise("Currently only 2 families are supported")
     remaining_goods=set(goods)
-    family1.discard_goods()
-    family2.discard_goods()
+    bundles = [set() for f in families]
 
     turn_index = 0
-    while True:
-        if len(remaining_goods) == 0: break
+    family_index = 0
+    while len(remaining_goods) > 0:
         turn_index += 1
-        allocate.trace("\nTurn #{}: group 1's turn to pick a good from {}:".format(turn_index, sorted(remaining_goods)))
-        g = family1.choose_good(remaining_goods)
-        allocate.trace("Group 1 picks "+g)
-        family1.take_good(g)
+        family_index = (family_index+1) % n_families
+        allocate.trace("\nTurn #{}: group {}'s turn to pick a good from {}:".format(turn_index, family_index, sorted(remaining_goods)))
+        current_family = families[family_index-1]
+        current_family_bundle = bundles[family_index-1]
+        g = current_family.choose_good(current_family_bundle, remaining_goods)
+        allocate.trace("Group {} picks {}".format(family_index,g))
+        current_family_bundle.add(g)
         remaining_goods.remove(g)
-        if len(remaining_goods) == 0: break
-        turn_index += 1
-        allocate.trace("\nTurn #{}: group 2's turn to pick a good from {}:".format(turn_index, sorted(remaining_goods)))
-        g = family2.choose_good(remaining_goods)
-        allocate.trace("Group 2 picks "+g)
-        family2.take_good(g)
-        remaining_goods.remove(g)
+    return bundles
 allocate.trace = lambda *x: None  # To enable tracing, set allocate.trace=True
 
 
-def allocate_enhanced(family1: BinaryFamily, family2: BinaryFamily, goods: set, threshold: float):
+def allocate_enhanced(families:list, goods: set, threshold: float):
     """
-    Run the Enhanced-RWAV protocol (see Section 3 in the paper) on the two given families, with the given threshold.
-    Set family1.allocated_goods and family2.allocated_goods to their final bundles.
+    Run the Enhanced RWAV protocol (see Section 3 in the paper) on the given families.
+    Currently only 2 families are supported.
+    Return a list of bundles - a bundle per family.
     :param threshold: a number in [0,1].
       If there is a single good g that is wanted by at least this fraction of members in one of the families,
       then this family gets g and the other family gets the other goods.
@@ -201,39 +182,39 @@ def allocate_enhanced(family1: BinaryFamily, family2: BinaryFamily, goods: set, 
     >>> fairness_1_of_best_2 = fairness_criteria.one_of_best_c(2)
     >>> family1 = BinaryFamily([BinaryAgent({"w","x"},1),BinaryAgent({"x","y"},3),BinaryAgent({"y","z"},3), BinaryAgent({"w","v"},3)], fairness_1_of_best_2)
     >>> family2 = BinaryFamily([BinaryAgent({"w","x"},5),BinaryAgent({"y","z"},5)], fairness_1_of_best_2)
-    >>> allocate_enhanced(family1, family2, ["v","w","x","y","z"], threshold=0.6)
-    >>> sorted(family1.allocated_goods)
+    >>> (bundle1,bundle2) = allocate_enhanced([family1, family2], ["v","w","x","y","z"], threshold=0.6)
+    >>> sorted(bundle1)
     ['y']
-    >>> sorted(family2.allocated_goods)
+    >>> sorted(bundle2)
     ['v', 'w', 'x', 'z']
-    >>> allocate_enhanced(family2, family1, ["v","w","x","y","z"], threshold=0.6)
-    >>> sorted(family1.allocated_goods)
+    >>> (bundle2,bundle1) = allocate_enhanced([family2, family1], ["v","w","x","y","z"], threshold=0.6)
+    >>> sorted(bundle1)
     ['y']
-    >>> sorted(family2.allocated_goods)
+    >>> sorted(bundle2)
     ['v', 'w', 'x', 'z']
     """
-    family1.discard_goods()
-    family2.discard_goods()
+    if len(families)!=2:
+        raise("Currently only 2 families are supported")
+
+    bundles = [set() for f in families]
     goods = set(goods)
 
-    threshold1 = threshold*family1.num_of_members()
-    threshold2 = threshold*family2.num_of_members()
+    thresholds = [threshold*family.num_of_members() for family in families]
     for g in goods:
-        num1 = family1.num_of_members_who_want(g)
-        num2 = family2.num_of_members_who_want(g)
-        if num1 >= threshold1:
-            family1.take_good(g)
-            family2.take_goods(goods.difference(set(g)))
+        nums = [family.num_of_members_who_want(g) for family in families]
+        if nums[0] >= thresholds[0]:
+            bundle1 = set(g)
+            bundle2 = goods.difference(bundle1)
             allocate_enhanced.trace("{} out of {} members in group 1 want {}, so group 1 gets {} and group 2 gets the rest".format(
-                                     num1,     family1.num_of_members(),     g,                  g))
-            return
-        elif num2 >= threshold2:
-            family2.take_good(g)
-            family1.take_goods(goods.difference(set(g)))
+                nums[0],     families[0].num_of_members(),     g,                  g))
+            return (bundle1,bundle2)
+        elif nums[1] >= thresholds[1]:
+            bundle2 = set(g)
+            bundle1 = goods.difference(bundle2)
             allocate_enhanced.trace("{} out of {} members in group 2 want {}, so group 2 gets {} and group 1 gets the rest".format(
-                num2,family2.num_of_members(), g, g))
-            return
-    allocate(family1, family2, goods)
+                nums[1],     families[1].num_of_members(),     g,                  g))
+            return (bundle1,bundle2)
+    allocate(families, goods)
 allocate_enhanced.trace = lambda *x: None  # To enable tracing, set allocate.trace=True
 
 
